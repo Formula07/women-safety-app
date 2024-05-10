73bb40533ce8853094d6c9ccd2f3fb391cf75894
Formula07 <110989642+Formula07@users.noreply.github.com>
2024-05-10 18:11:28 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "women safety test 1"
+include(":app")
+ 
